b48bb746bf8fca6e59093809fb330c714bc85e44
Emmanuel LINGUET <65608769+xmanu91@users.noreply.github.com>
2022-03-21 20:17:11 +0100

    Update meme.sh

--- meme.sh
+++ meme.sh
@@ -1,1 +1,3 @@
-firefox -url http://randommemer.herokuapp.com/ & xdotool search --sync --onlyvisible --class "Firefox" windowactivate key F11
+git clone http://github.com/xmanu91/Meme-viewer-Raspberry-Pi.git meme-viewer
+cd meme-viewer/
+firefox index.html & xdotool search --sync --onlyvisible --class "Firefox" windowactivate key F11
