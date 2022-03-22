cde3895af7b90789d52e4480c686cb12419088c4
Emmanuel LINGUET <65608769+xmanu91@users.noreply.github.com>
2022-03-22 18:12:11 +0100

    Update meme.sh

--- meme.sh
+++ meme.sh
@@ -1,4 +1,4 @@
-cd meme-viewer/
-git pull
+rm -rf meme-viewer/
+git clone https://github.com/xmanu91/Meme-viewer-Raspberry-Pi.git meme-viewer
+cd meme-viewer
 firefox index.html & xdotool search --sync --onlyvisible --class "Firefox" windowactivate key F11
-echo "tst"
